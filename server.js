29e75d8c3721045c187fba9a655c76bb908b68df
asmon <oscarsolideo@gmail.com>
2022-01-28 11:51:47 +0700

    edit file

---
+++ server.js
@@ -0,0 +1,15 @@
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+const express = require('express');
+const app = express();
+app.use(express.static('./dist/apps/teacher'));
+app.use(requireHTTPS);
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', { root: 'dist/apps/teacher/' });
+});
+app.listen(process.env.PORT || 8080);
